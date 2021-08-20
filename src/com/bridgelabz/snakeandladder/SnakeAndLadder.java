42e77570802db16b1470f7c789cee9bf6148a68c
namancg <naman.c@perfios.com>
2021-08-21 01:31:18 +0530

    ADDED: uc3-PlayerCheck

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -3,15 +3,29 @@ package com.bridgelabz.snakeandladder;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
-
 		int pos = 0;
 		int dice = (int) Math.floor(Math.random()*100)%6+1;
+		int option = (int) Math.floor(Math.random()*100)%3;
+		
+		switch(option) {
+		
+		case 0: 
+			System.out.println("NO PLAY!");
+			break;
+			
+		case 1:
+			System.out.println("LADDER");
+			pos+=dice;
+			break;
+			
+		case 2:
+			System.out.println("SNAKE");
+			pos-=dice;
+			break;
+		
+		}
 		System.out.println("Dice Rolled : "+dice);
-		pos+=dice;
 		System.out.println("Current Position : "+pos);
 
-
-	}
-
-
+}
 }
