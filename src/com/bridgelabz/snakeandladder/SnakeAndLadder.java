dc8e820db2ad2c4fdf6adaa05bdbbeb22f2febd9
namancg <naman.c@perfios.com>
2021-08-21 01:28:10 +0530

    ADDED: uc2-1to6

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -5,12 +5,13 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 
 		int pos = 0;
-		
+		int dice = (int) Math.floor(Math.random()*100)%6+1;
+		System.out.println("Dice Rolled : "+dice);
+		pos+=dice;
 		System.out.println("Current Position : "+pos);
 
-	}
-
 
 	}
 
 
+}
