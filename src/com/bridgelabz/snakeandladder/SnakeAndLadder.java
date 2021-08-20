83ccc86460501cfb14bc6b4c2b35b1a053100bcf
namancg <naman.c@perfios.com>
2021-08-21 01:39:49 +0530

    ADDED: uc6-noOfDice

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -5,10 +5,12 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 
 		int pos = 0;
+		int total_dice_rolls = 0;
 		
 		while(pos<100) {
 			
 			int dice = (int) Math.floor(Math.random()*100)%6+1;
+			total_dice_rolls++;
 			int option = (int) Math.floor(Math.random()*100)%3;
 			
 			switch(option) {
@@ -34,9 +36,11 @@ public class SnakeAndLadder {
 			}
 			System.out.println("Rolled Dice: "+dice);
 			System.out.println("Current Position : "+pos);
+			System.out.println(total_dice_rolls+" DICE ROLL...");
 			
 		}
 		
-		System.out.println("WINNER!");
+		System.out.println("WINNER");
+		System.out.println(" TOOK "+total_dice_rolls+"NO OF DICE ROLLS TO WIN");
 }
 }
