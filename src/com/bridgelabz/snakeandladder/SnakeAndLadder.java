b8d606528d50836b6bf6a3bd328c653a3403207c
namancg <naman.c@perfios.com>
2021-08-21 01:26:42 +0530

    ADDED: uc1-StartPos

---
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.snakeandladder;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+
+		int pos = 0;
+		
+		System.out.println("Current Position : "+pos);
+
+	}
+
+
+	}
+
+
